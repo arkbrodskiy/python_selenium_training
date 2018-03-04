3754b948541c7eb99398f262f7561339a86bf091
Arkadiy Brodskiy <arkbrod@gmail.com>
2018-03-04 09:40:17 -0500

    Задание 03.1. Тест для добавления контакта записан в Selenium Builder и экспортирован в среду разработки

---
+++ test_add_contact.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium.webdriver.common.action_chains import ActionChains
+import time, unittest
+
+
+def is_alert_present(wd):
+    try:
+        wd.switch_to_alert().text
+        return True
+    except:
+        return False
+
+
+class TestAddContact(unittest.TestCase):
+    def setUp(self):
+        self.wd = WebDriver(capabilities={"marionette": False},
+                            firefox_binary="C:/Program Files/Mozilla Firefox/firefox.exe")
+        self.wd.implicitly_wait(60)
+    
+    def test_add_contact(self):
+        wd = self.wd
+        wd.get("http://localhost/addressbook/")
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_css_selector("[type='submit']").click()
+        wd.find_element_by_link_text("add new").click()
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys("First_name")
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys("Last_name")
+        wd.find_element_by_name("submit").click()
+        wd.find_element_by_link_text("home page").click()
+        wd.find_element_by_link_text("Logout").click()
+
+    def tearDown(self):
+        self.wd.quit()
+
+if __name__ == '__main__':
+    unittest.main()
