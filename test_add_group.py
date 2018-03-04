b66f07fb1601e0a56fe8be6859e2a116d7a0f901
Arkadiy Brodskiy <arkbrod@gmail.com>
2018-03-04 09:25:10 -0500

    Задание 02. Заменен селектор для кнопки "Login"

--- test_add_group.py
+++ test_add_group.py
@@ -53,7 +53,7 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+        wd.find_element_by_css_selector("[type='submit']").click()
 
     def open_home_page(self, wd):
         wd.get("http://localhost/addressbook/")
