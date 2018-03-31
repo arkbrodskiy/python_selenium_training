b5d8cc7a61bc8ad1757638d9176da1bbc815f51f
Arkadiy Brodskiy <arkbrod@gmail.com>
2018-03-31 14:35:24 -0400

    Задание 15.1. Реализован тест для создания групп, в котором используются случайно сгенерированные строки.

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,9 +1,24 @@
+import random
+import string
+
 from model.group import Group
+import pytest
 
 
-def test_add_group(app):
+def random_string(prefix, max_len):
+    symbols = string.ascii_letters + string.digits + " " * 10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(max_len))])
+
+
+test_data = [Group(name=random_string('name', 10),
+                   header=random_string('header', 20),
+                   footer=random_string('footer', 20)),
+             Group(name="", header="", footer="")]
+
+
+@pytest.mark.parametrize("group", test_data, ids=[repr(x) for x in test_data])
+def test_add_group(app, group):
     old_groups = app.group.get_group_list()
-    group = Group(name="Python group 01", header="p header 01", footer="p footer 01")
     app.group.create(group)
     assert len(old_groups) + 1 == app.utils.count()
     new_groups = app.group.get_group_list()
