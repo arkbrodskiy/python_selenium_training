31f9f78984d6adf45ea1c3109281bc36e236f7de
Arkadiy Brodskiy <arkbrod@gmail.com>
2018-04-07 11:00:52 -0400

    Задание 17.1. Реализована загрузка информации из конфигурационный файл с автоматическим определением пути к этому конфигурационному файлу.

--- conftest.py
+++ conftest.py
@@ -1,30 +1,35 @@
+import json
 import pytest
-
 from fixture.application import Application
+import os.path
 
 fixture = None
+target = None
 
 
 @pytest.fixture
 def app(request):
     global fixture
-    if fixture is None or not fixture.is_valid():
-        browser = request.config.getoption("--browser")
-        base_url = request.config.getoption("--base_url")
-        fixture = Application(browser=browser, base_url=base_url)
-    fixture.session.ensure_login(username="admin", password="secret")
+    global target
+    browser = request.config.getoption('--browser')
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption('--target'))
+        with open(config_file) as file:
+            target = json.load(file)
+        if fixture is None or not fixture.is_valid():
+            fixture = Application(browser=browser, base_url=target['base_url'])
+    fixture.session.ensure_login(username=target['username'], password=target['password'])
     return fixture
 
 
-@pytest.fixture(scope="session", autouse=True)
+@pytest.fixture(scope='session', autouse=True)
 def stop(request):
     def fin():
         fixture.session.ensure_logout()
         fixture.destroy()
-
     request.addfinalizer(fin)
 
 
 def pytest_addoption(parser):
-    parser.addoption("--browser", action="store", default="chrome")
-    parser.addoption("--base_url", action="store", default="http://localhost/addressbook/")
+    parser.addoption('--browser', action='store', default='chrome')
+    parser.addoption('--target', action='store', default='target.json')
