ff4bef22512d99c788dd3413eddff4a6eb214eb4
Arkadiy Brodskiy <arkbrod@gmail.com>
2018-05-06 16:56:01 -0400

    21.2. Исправлено название тестового метода для проверки контактов на главной странице.

--- test/test_verify_contact.py
+++ test/test_verify_contact.py
@@ -1,6 +1,6 @@
 
 
-def test_verify_random_contact_on_home_page(app, orm):
+def test_verify_all_contacts_on_home_page(app, orm):
     if len(orm.get_contact_list()) == 0:
         app.contact.create_one_contact()
     contacts_ui = sorted(app.contact.get_contact_list(), key=app.utils.id_or_max)
