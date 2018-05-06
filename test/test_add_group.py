8e53600d8c3ddf051fdf3ce26b2682c49047e246
Arkadiy Brodskiy <arkbrod@gmail.com>
2018-05-06 16:18:10 -0400

    29.1. Реализована генерация отчетов при помощи Allure

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,13 +1,18 @@
+import pytest
 
 
 def test_add_group(app, orm, json_groups, check_ui):
     group = json_groups
-    old_groups = orm.get_group_list()
-    app.group.create(group)
-    new_groups = orm.get_group_list()
-    old_groups.append(group)
-    assert sorted(old_groups, key=app.utils.id_or_max) == sorted(new_groups, key=app.utils.id_or_max)
-    if check_ui:
-        print('UI verification activated')
-        assert sorted(new_groups, key=app.utils.id_or_max) == sorted(app.group.get_group_list(), key=app.utils.id_or_max)
+    with pytest.allure.step('Given a group list'):
+        old_groups = orm.get_group_list()
+    with pytest.allure.step('When I add the group %s to the list' % group):
+        app.group.create(group)
+    with pytest.allure.step('Then the new group list is equal to the old list with the added group'):
+        new_groups = orm.get_group_list()
+        old_groups.append(group)
+        assert sorted(old_groups, key=app.utils.id_or_max) == sorted(new_groups, key=app.utils.id_or_max)
+        if check_ui:
+            print('UI verification activated')
+            assert sorted(new_groups, key=app.utils.id_or_max) == \
+                sorted(app.group.get_group_list(), key=app.utils.id_or_max)
 
